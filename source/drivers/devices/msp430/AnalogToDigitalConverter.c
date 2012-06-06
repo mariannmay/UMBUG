b79026e2350ed21c7d5aecb8ceba25200d0275f2
Kane Anderson <kane.g.anderson@gmail.com>
2012-06-06 17:45:36 -0500

    just moved comments... no change
    Signed-off-by: Kane Anderson <kane.g.anderson@gmail.com>

--- source/drivers/devices/msp430/AnalogToDigitalConverter.c
+++ source/drivers/devices/msp430/AnalogToDigitalConverter.c
@@ -22,30 +22,27 @@ void initialize_analogToDigitalConverter(void)
 									// the first sample is ADC12MEM0
     ADC12CTL1 |= (SHP+CONSEQ_3);	// conversion mode is 'repeat sequence of channels' (page 28-12)
     ADC12CTL0 = (ADC12ON+MSC);
-							
-							
-							
-							
-							
-	ADC12MCTL0 = 0x00;		// makes A0 go to MEM0, with V+ = Vcc and V- = Vss
-	ADC12MCTL1 = 0x01;		// makes A1 go to MEM1, with V+ = Vcc and V- = Vss
-	ADC12MCTL2 = 0x02;		// makes A2 go to MEM2, with V+ = Vcc and V- = Vss
-	ADC12MCTL3 = 0x03;		// makes A3 go to MEM3, with V+ = Vcc and V- = Vss
-	ADC12MCTL4 = 0x04;		// makes A4 go to MEM4, with V+ = Vcc and V- = Vss
-	ADC12MCTL5 = 0x05;		// makes A5 go to MEM5, with V+ = Vcc and V- = Vss
-	ADC12MCTL6 = 0x06;		// makes A6 go to MEM6, with V+ = Vcc and V- = Vss
-	ADC12MCTL7 = 0x07;		// makes A7 go to MEM7, with V+ = Vcc and V- = Vss
-	ADC12MCTL8 = 0x08;		// makes VeRef+ go to MEM8, with V+ = Vcc and V- = Vss
-	ADC12MCTL9 = 0x09;		// makes VRef-/VeRef- go to MEM9, with V+ = Vcc and V- = Vss
-	ADC12MCTL10 = 0x0A;		// makes temperature sensor go to MEM10, with V+ = Vcc and V- = Vss
-	ADC12MCTL11 = 0x0B;		// makes (Vcc - Vss)/2 go to MEM11, with V+ = Vcc and V- = Vss
-	ADC12MCTL12 = 0x0C;		// makes A12 go to MEM12, with V+ = Vcc and V- = Vss
-	ADC12MCTL13 = 0x0D;		// makes A13 go to MEM13, with V+ = Vcc and V- = Vss
-	ADC12MCTL14 = 0x0E;		// makes A14 go to MEM14, with V+ = Vcc and V- = Vss
-	ADC12MCTL15 = 0x0F+EOS;	// makes A15 go to MEM15, with V+ = Vcc and V- = Vss
-	ADC12IE = 0x00;			// disable analog to digital interrupts... we'll get to them soon enough
-	ADC12CTL0 = 0x00F3; 	// see the user guide page 28-22 for interpretation
-	P6SEL = 0xFF; 			// sets port 6 to use all 8 A2D
+    
+   	
+	ADC12MCTL0	= 0x00;				// makes A0 go to MEM0, with V+ = Vcc and V- = Vss
+	ADC12MCTL1	= 0x01;				// makes A1 go to MEM1, with V+ = Vcc and V- = Vss
+	ADC12MCTL2	= 0x02;				// makes A2 go to MEM2, with V+ = Vcc and V- = Vss
+	ADC12MCTL3	= 0x03;				// makes A3 go to MEM3, with V+ = Vcc and V- = Vss
+	ADC12MCTL4	= 0x04;				// makes A4 go to MEM4, with V+ = Vcc and V- = Vss
+	ADC12MCTL5	= 0x05;				// makes A5 go to MEM5, with V+ = Vcc and V- = Vss
+	ADC12MCTL6	= 0x06;				// makes A6 go to MEM6, with V+ = Vcc and V- = Vss
+	ADC12MCTL7	= 0x07;				// makes A7 go to MEM7, with V+ = Vcc and V- = Vss
+	ADC12MCTL8	= 0x08;				// makes VeRef+ go to MEM8, with V+ = Vcc and V- = Vss
+	ADC12MCTL9	= 0x09;				// makes VRef-/VeRef- go to MEM9, with V+ = Vcc and V- = Vss
+	ADC12MCTL10	= 0x0A;				// makes temperature sensor go to MEM10, with V+ = Vcc and V- = Vss
+	ADC12MCTL11	= 0x0B;				// makes (Vcc - Vss)/2 go to MEM11, with V+ = Vcc and V- = Vss
+	ADC12MCTL12	= 0x0C;				// makes A12 go to MEM12, with V+ = Vcc and V- = Vss
+	ADC12MCTL13	= 0x0D;				// makes A13 go to MEM13, with V+ = Vcc and V- = Vss
+	ADC12MCTL14	= 0x0E;				// makes A14 go to MEM14, with V+ = Vcc and V- = Vss
+	ADC12MCTL15	= 0x0F+EOS;			// makes A15 go to MEM15, with V+ = Vcc and V- = Vss
+	ADC12IE		= 0x00;				// disable analog to digital interrupts... we'll get to them soon enough
+	ADC12CTL0	= 0x00F3; 			// see the user guide page 28-22 for interpretation
+	P6SEL		= 0xFF; 			// sets port 6 to use all 8 A2D
 }
 
 //////////////////////////////////////////////////////////////////
