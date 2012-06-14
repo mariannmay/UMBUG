fd12a2f1748feb9e57030995a9710d05e2920922
Chen <tianclch@hotmail.com>
2012-06-14 03:03:10 -0500

    create skull files for basic matrix operations

---
+++ source/application/ADC/algorithm/Matrix_basic.h
@@ -0,0 +1,11 @@
+/**************************************
+  Matrix basic operations header file
+  Author: Chen
+  Date: June 14 2012
+
+  Comment: function will be tested on local computer first then upload to repo
+  *************************************/
+
+#ifndef MATRIX_BASIC_H
+#define MATRIX_BASIC_H
+#endif
