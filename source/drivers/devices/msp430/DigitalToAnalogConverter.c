0a1f2ce4e7f488a60a9c6692466867f3152ead1d
Kane Anderson <kane.g.anderson@gmail.com>
2012-06-26 18:39:29 -0500

    - did skeleton code for D to A converter driver
    Signed-off-by: Kane Anderson <kane.g.anderson@gmail.com>

---
+++ source/drivers/devices/msp430/DigitalToAnalogConverter.c
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////
+//                                                              //
+//    representation of the processor's D2A                     //
+//    last edited by: Kane Anderson                             //
+//    date: June 26, 2012                                    	//
+//																//
+//////////////////////////////////////////////////////////////////
+
+#include "./DigitalToAnalogConverter.h"
+
+//////////////////////////////////////////////////////////////////
+
+void initialize_digitalToAnalogConverter(void)
+{
+	// TODO
+}
+
+//////////////////////////////////////////////////////////////////
+
+void startNewDigitalToAnalogConversion(Word digital_12_bit_value)
+{
+	// TODO
+}
+
+
--- source/test application/TestApplication.c
+++ source/test application/TestApplication.c
@@ -36,25 +36,6 @@ void test_application_main(void)
 
 void test_COM(void)
 {
-	/*
-	Data D;
-	//malloc required?
-	D.size = 3;
-	D.type = true;
-	D.index[0] = 'a';
-	D.index[1] = 'b';
-	D.index[2] = 'c';
-
-	Packet P;
-	//malloc here?
-	P.size = D.size+20;
-	
-	char dest[] = {'V','E','4','U','M','_','1'};
-	packetize(&D, &P, dest); //stuffing info into an AX.25 packet for VE4UM_1
-	
-	//heres where I want to test each byte
-	//assert(pack[17] == 'a');
-	*/
 	
 	P5DIR |= BIT1;                // P5.1 as output
   	P5OUT |= BIT1;                // P5.1 set high
@@ -63,9 +44,6 @@ void test_COM(void)
 
 	#if CDH_PROCESSOR_COMPILE
 		P3OUT &= ~0x01;		// slave enable
-		//P3OUT |= 0x01; //start with slave disabled
-		//spiSendByte('X');
-		//unsigned int x =0;
 		
 		char nextCharToSend = '1';
 		for(;;nextCharToSend++)
@@ -73,160 +51,41 @@ void test_COM(void)
 			initialize_SPI(1);
 			if (nextCharToSend == ':') nextCharToSend = '1';
 			
-			//P5OUT ^= BIT1;              //always Toggle P5.1 if master
 			P3OUT &= ~0x01; //set STE low for slave enable
 			spiSendByte(nextCharToSend);
-			//Byte array[4] = {'k','a','n','e'};
-			//spiSendFrame(array, 4);
 			
 			P3OUT |= 0x01; //set STE high for slave disable
-			//spiSendByte('R');
-			//spiSendByte('A');
-			//spiSendByte('I');
-			//spiSendByte('G');
 			
 			// just a time killing loop
 			int waitTimer = 10000;
 			for (waitTimer; waitTimer > 0; waitTimer--){ ; }
 			
-			///////////////////////////////////////////////////
-			/*
-			printf("wait for TX \n");
-			fflush(stdout);			
-			while (halSPITXREADY ==0);    // wait while not ready 
-			IFG2 &= ~UCB0RXIFG;
-			P3OUT &= ~0x01; //set STE low for slave enable
-			printf("wait for enable \n");
-			fflush(stdout);
-			while(P3IN&0x10);//wait for MTE to be pulled low
-			halSPI_SEND('C');            // write
-			printf("wait for RX \n");
-			fflush(stdout);
-			while (halSPIRXREADY ==0);    // wait for RX buffer (full)
-			char buff = UCB0RXBUF;
-			printf("recieved: %c\n", buff);
-			fflush(stdout);
-			P3OUT |= 0x01; //set STE high for slave disable
-			printf("wait for disable \n");
-			fflush(stdout);
-			while(~(P3IN&0x10));//wait for MTE to be pulled high	
-			*/	
-			  //return (halSPIRXBUF);
-			///////////////////////////////////////////////////
-			
-			/*if(x == 10000)
-			{
-				P5OUT ^= BIT1;
-				x=0;
-			}      // Delay
-			else
-			{
-				x++;
-			}*/
-			//P3OUT ^= 0x08;
 		}
 	#else
 		
-		//unsigned int x =0;
-		//P3OUT |= 0x10; //disable MTE off the start
 		printf("    COM Slave Initialize\r\n");
 		printf("Searching for: %c\n", 'C');
 		fflush(stdout);
-		//for(;;)
-		//{
-			
-			//P5OUT ^= BIT1;              //always Toggle P5.1 if master
-			//while((P3IN & 0x01) == 0x01);	//wait for enable
-			//char buff = spiSendByte(0xE1);		
-			//while((P3IN & 0x01) == 0x00);	//wait for disable
-			
-			/////////////////////////////////////////////////////////////////// 
-			/*
-			printf("wait for enable %u\n", x);
-			fflush(stdout);
-			while((P3IN & 0x01) == 0x01);	//wait for enable
-			printf("wait for TXBUFF %u\n", x);
-			fflush(stdout);
-			while (halSPITXREADY ==0);    // wait while not ready
-			printf("wait for disable %u\n", x); 
-			fflush(stdout);
-			IFG2 &= ~UCB0RXIFG;
-			halSPI_SEND('N');            // write
-			P3OUT &= ~0x10;// pull MTE low
-			while((P3IN & 0x01) == 0x00);	//wait for disable
-			printf("wait for RXBUFF %u\n", x);
-			fflush(stdout);
-			while (halSPIRXREADY ==0);    // wait for RX buffer (full)
-			char buff = UCB0RXBUF;
-			printf("recieved: %c\n", buff);
-			fflush(stdout);
-			P3OUT |= 0x10;// pull MTE high
-			*/
-			///////////////////////////////////////////////////////////////////
-			
-			/*if(x == 10000)
-			{
-				P5OUT ^= BIT1;
-				x=0;
-			}      // Delay
-			else
-			{
-				x++;
-			}*/
 
-			//if(buff=='C')
-			//{
-			//	P5OUT &= ~BIT1;
-			//}
-
-		//}
-	  for(;;)
-	  {
-	  	initialize_SPI(0);
-	  	UCB0TXBUF = 0xFF;
-		while((P3IN & 0x01) == 0x01);	//wait for enable
-		bool enabled = ((P3IN | 0xFE) == 0xFE);
-		if (enabled)
-		{
-			//while (halSPITXREADY ==0);   // wait while not ready for TX
-			//halSPI_SEND(0xFF);     // dummy write
-			while (halSPIRXREADY ==0);   // wait for RX buffer (full)
-			char buff = UCB0RXBUF;
-			printf("recieved: %c\n", buff);
-			fflush(stdout);
-		}
-		//while((P3IN & 0x01) == 0x00);	//wait for disable
-	  }
-		//if(buff==0xF5)
-		//{
-		//	P5OUT &= ~BIT1;
-		//}
-		//{
-			//if((P3IN & 0x01) == 0x00)//check STE low
-			//{
-			//	P5OUT ^= BIT1; //if connected to master toggle LED
-			//}
-			
-			//unsigned int x;
-			//for(x=50000;x>0;x--);
-		//}
-	  //}
-	  /*
 		for(;;)
 		{
-			char buff = spiSendByte(0x55);
-			if(buff==0x55)
+		  	initialize_SPI(0);
+		  	UCB0TXBUF = 0xFF;
+			while((P3IN & 0x01) == 0x01);	//wait for enable
+			bool enabled = ((P3IN | 0xFE) == 0xFE);
+			if (enabled)
 			{
-				P5OUT ^= BIT1; //if connected to master toggle LED
+				while (halSPIRXREADY ==0);   // wait for RX buffer (full)
+				char buff = UCB0RXBUF;
+				printf("recieved: %c\n", buff);
+				fflush(stdout);
 			}
 		}
-		*/
+		
 	#endif
 	
-	//fflush(stdout);
-	
-	//printf("    COM test complete\r\n");
-	//fflush(stdout);
+	printf("    COM test complete\r\n");
+	fflush(stdout);
 }
 
 ///////////////////////////////////////////////////////////////////
