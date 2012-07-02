91a7f00f6964d537bdb7374ad0d08a0577f930f5
Chen <tianclch@hotmail.com>
2012-07-02 16:19:55 -0500

    add matrix multiplication

--- source/application/ADC/algorithm/Matrix.h
+++ source/application/ADC/algorithm/Matrix.h
@@ -1,6 +1,23 @@
+//////////////////////////////////////////////////////////////////
+//                                                              //
+//    Matrix header                                             //
+//    last edited by:  Chen Liu                                 //
+//    date: July 2, 2012                                        //
+//                                                              //
+//////////////////////////////////////////////////////////////////
+
 #ifndef MATRIX_H
 #define MATRIX_H
 
-void matrix_add(int M1[3][3],int M2[3][3],int result[3][3]);
+#include"../../../SimpleDefinitions.h"
+/*********************/
+extern void matrix_add(UI16 m1[3][3],UI16 m2[3][3],UI16 result[3][3]);
+extern void matrix_multiply(UI16 m1[3][3],UI16 m2[3][3],UI16 result[3][3]);
+
+//TODO:remove 
+extern void printMatrix(UI16 m[3][3]);
+
+/*********************/
+static void zeroMatrix(UI16 m[3][3]);
 
 #endif
