ef3e3d189de9be491ae752a8593356b0a9c357b1
Troy Denton <trdenton@gmail.com>
2012-09-10 20:19:18 -0500

    added memset functions

---
+++ source/system/MemSet.h
@@ -0,0 +1,13 @@
+/*
+ * These functions set different variable sizes.
+ * they return 1 on success or 0 on failure.
+ * 
+ */
+void setMemoryAt_int(int*,int);
+void setMemoryAt_UI8(UI8*, UI8);
+void setMemoryAt_UI16(UI16*,UI16);
+void setMemoryAt_UI32(UI32*,UI32);
+void setMemoryAt_TimeCounter(TimeCounter*, TimeCounter);//prob not necessary?
+
+
+//set arbitrary data by using UI8's.
