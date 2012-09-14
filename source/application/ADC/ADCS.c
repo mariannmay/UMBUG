f8f6e31bbd19ef0206398c9a77561a378d56034c
Matt Woelk <matt.woelk@gmail.com>
2012-09-13 21:23:54 -0500

    ADCS skeleton code.

---
+++ source/application/ADC/ADCS.c
@@ -0,0 +1,9 @@
+void adcs_routine(void){
+	//TODO: This is the routine which will run during normal operations
+	//      to keep the satellite pointing correctly.
+}
+
+void adcs_LEOP(void){
+	//TODO: This is the routine which runs right after launch
+	//      to get the satellite out of a tumble. (B-DOT)
+}
