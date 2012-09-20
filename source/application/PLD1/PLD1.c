1e9a5411b168c5bc92712d1c129025b304e86b82
Matt Woelk <matt.woelk@gmail.com>
2012-09-19 20:47:34 -0500

    Re-wrote PLD1.c, PLD2.c, and THM.c. Added ground station identifier.

--- source/application/PLD1/PLD1.c
+++ source/application/PLD1/PLD1.c
@@ -10,12 +10,12 @@ void pld1_routine(void)
 	if (getUARTState() == UART_CAMorSPEC)
 	{	
 		//we're in the middle of sending things.
-		if (0 /*we've sent the whole image OR we have timed out*/)
+		if (0 /*TODO: we've sent the whole image OR TODO: we have timed out*/)
 		{
 			setUARTState(UART_NOT_RESERVED); // unset the flag to say we are done with the UART
 		}
 	}
-	else if (getUARTState() == UART_NOT_RESERVED && 0 /*enough time since last run*/)
+	else if (getUARTState() == UART_NOT_RESERVED && 0 /*TODO: enough time since last run*/)
 	{
 		setUARTState(UART_CAMorSPEC); //claim the UART
 		Packetize(PKT_TARDIGRADEIMAGEMODE1, bytesToSend, PLD1Bytes); // start sending the data to the packetizer to be timestamped
